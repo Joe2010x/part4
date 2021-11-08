59d5d4f0ce50e582ab4bbeb90448a0426b988907
Dongping Sun <dongping.d.sun@utu.fi>
2021-11-08 19:48:27 +0200

    part4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -9,9 +9,31 @@ const totalLikes = (blogs) =>{
     const result = blogs.reduce((sum,obj)=>{
         return sum+obj["likes"]
     },0)
-  
 
     return result;
 }
+
+const favoriteBlog = (blogs) => {
+    let maxLikes = 0;
+    let favBlog ={
+        title:'',
+        author:'',
+        likes:0
+    } ;
+    const result = blogs.forEach(obj => {
+        
+        if (maxLikes < obj["likes"]) {
+            maxLikes  = obj["likes"] 
+            favBlog = {
+                title:obj.title,
+                author:obj.author,
+                likes:obj.likes
+            }
+            
+              
+        }
+    })
+    return favBlog
+}
   
-  module.exports = {dummy,totalLikes}
+  module.exports = {dummy,totalLikes, favoriteBlog}
